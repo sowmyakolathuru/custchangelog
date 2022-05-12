475bbee8bc2b90debdda325ea4169ac95b719c71
sowmyakolathuru <sowmya.kolathuru@intellectdesign.com>
2022-05-12 17:25:00 +0530

    Update and rename changelogcust_h3.sql to changelogcust_h4.sql

--- changelogcust_h3.sql
+++
@@ -1,4 +0,0 @@
---liquibase formatted sql
-
---changeset rom:118
-INSERT INTO customers VALUES(1,'Sani',43,'Chennai',300000);
---
+++ changelogcust_h4.sql
@@ -0,0 +1,4 @@
+--liquibase formatted sql
+
+--changeset qom:200
+INSERT INTO customers VALUES(2,'tani',24,'Mumbai',300000);
